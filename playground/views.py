a4d6cdfff2a6b994db373ae6dde0c8e04255bcd5
Drenii <dreni107@hotmail.com>
2025-03-14 03:22:16 +0100

    working with data

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 def say_hello(request):
+    query_set = Product.objects.all()
+    for product in query_set:
+        print(product.title)
+
+    # print(query_set[0:5]) show first 5 rows
+  
+    # list(query_set.values()) show them on the list
     return render(request, 'hello.html', {'name': 'Dren'})
