256c874bdafe5ac2ed444643a11666eb7cacb736
Drenii <dreni107@hotmail.com>
2025-03-16 02:28:38 +0100

    adding Q object

--- playground/views.py
+++ playground/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 
 def say_hello(request):
     # query_set = Product.objects.all()
     # for product in query_set:
     #     print(product.title)
     # query_set = Product.objects.filter(price__range=(20,30))
-    query_set = Product.objects.filter(title__icontains='coffee')
+    # query_set = Product.objects.filter(title__icontains='coffee')
+    # Q OBJECT EXAMPLE
+    query_set = Product.objects.filter(Q(inventory__lt=10) |Q(price__lt=20))
+    # ~Q is like not Q
 
 
     # print(query_set[0:5]) show first 5 rows
