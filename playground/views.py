b1af772ccc3491434ec72bb82091092e02ed6cbe
Drenii <dreni107@hotmail.com>
2025-03-16 02:39:27 +0100

    filter by order

--- playground/views.py
+++ playground/views.py
@@ -14,7 +14,9 @@ def say_hello(request):
     # query_set = Product.objects.filter(Q(inventory__lt=10) |Q(price__lt=20))
     # ~Q is like not Q
     # F referencing fields, example find all data from db where inventory is equat to price
-    query_set = Product.objects.filter(inventory=F('price'))
+    # query_set = Product.objects.filter(inventory=F('price'))
+    #filter by order
+    query_set = Product.objects.order_by('price', '-title').reverse()
 
     # print(query_set[0:5]) show first 5 rows
   
