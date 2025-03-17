acf0ee71cf1bf0b8433781b5cef7c7d8fb4e8744
Drenii <dreni107@hotmail.com>
2025-03-17 23:59:25 +0100

    working with db

--- playground/views.py
+++ playground/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from store.models import Product, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
+from django.db.models.aggregates import Max, Min, Count, Avg, Sum
 
 def say_hello(request):
     # query_set = Product.objects.all()
@@ -22,9 +23,10 @@ def say_hello(request):
     # SELECTING FIELDS TO QUERY
     # query_set=Product.objects.values_list('id', 'title', 'collection__title')
     # JOIN TWO TABLES
-    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-
+    # query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    result= Product.objects.aggregate(count=Count('id', min_price=Min('price')))
     # print(query_set[0:5]) show first 5 rows
   
     # list(query_set.values()) show them on the list
-    return render(request, 'hello.html', {'name': 'Dren', 'products': list(query_set)})
+    # return render(request, 'hello.html', {'name': 'Dren', 'products': list(query_set)})
+    return render(request, 'hello.html', {'name': 'Dren', 'results': result}) #this one is useed for aggregation purposes
